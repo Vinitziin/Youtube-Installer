499122c47cc81571ce209313e37d7eadab9bd3a0
Vinitziin <vinicius.tiago.65154@a.fecaf.com.br>
2024-12-13 15:32:12 -0300

    chore: service lógica para instalar o video do youtube

---
+++ backend/app/services/downloader.py
@@ -0,0 +1,18 @@
+import yt_dlp
+
+def baixar_video(url, formato_saida='mp4', qualidade='best'):
+    """
+    Baixa um vídeo do YouTube com base no link, formato e qualidade desejados.
+    """
+    try:
+        ydl_opts = {
+            'format': qualidade,
+            'outtmpl': './downloads/%(title)s.%(ext)s',
+            'merge_output_format': formato_saida,
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            arquivo_saida = ydl.prepare_filename(info)
+            return arquivo_saida
+    except Exception as e:
+        raise Exception(f"Erro ao baixar vídeo: {e}")
